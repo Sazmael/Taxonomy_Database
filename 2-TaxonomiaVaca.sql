9fb2cebb4660a029f56af13f36470445a6b28ca0
Olman Dávila Solano <85916689+Sazmael@users.noreply.github.com>
2022-05-14 21:38:37 -0600

    Update 2-TaxonomiaVaca.sql

--- 2-TaxonomiaVaca.sql
+++ 2-TaxonomiaVaca.sql
@@ -3,15 +3,19 @@ use Taxonomias;
 go
 
 /*Unir tablas para mostrar la taxonomia de vaca*/
-select Reino.idReino 
-from Reino 
+select Reino.idReino 'Reino',Phylum.idPhylum 'Phylum', Clase.idClase
+'Clase', Orden.idOrden 'Orden', Familia.idFamilia 'Familia',
+Genero.idGenero 'Genero',Especie.idEspecie
+'Especie',Especie.Nombre_Cientifico
+'Nombre_Cientifico',Especie.Nombre_Comun 'Nombre_Comun'
+from Reino
 join Phylum on Reino.idReino = Phylum.reino_idReino
-join Clase on Phylum.idPhylum = Clase.Phylum_idPhylum 
-join Orden on Clase.idClase = Orden.Clase_idClase 
-join Familia on  Orden.idOrden = Familia.Orden_idOrden 
+join Clase on Phylum.idPhylum = Clase.Phylum_idPhylum
+join Orden on Clase.idClase = Orden.Clase_idClase
+join Familia on Orden.idOrden = Familia.Orden_idOrden
 join Genero on Familia.idFamilia = Genero.Familia_idFamilia
 join Especie on Genero.idGenero = Especie.Genero_idGenero
-where Nombre_Comun='Maíz';
+where Nombre_Comun='Vaca';
 
 go
 
